78dd880853b692e7a2a5b447462355c860bb5637
Gozvn <mrgozvn@gmail.com>
2025-11-21 16:27:25 +0700

    Feautre : Add Title

--- frontend/src/app/modules/user/pages/post/post.component.ts
+++ frontend/src/app/modules/user/pages/post/post.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../../../layouts/default/partials/footer/footer';
+import { Header } from '../../../../layouts/default/partials/header/header';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
