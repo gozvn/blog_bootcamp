207bb161cf1b20987464c9c6a10a99e745ca5ad3
Gozvn <mrgozvn@gmail.com>
2025-11-22 11:34:16 +0700

    WIP : USer Add Post

--- frontend/src/app/modules/user/pages/post/post.component.ts
+++ frontend/src/app/modules/user/pages/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../../../layouts/default/partials/footer/footer';
 import { Header } from '../../../../layouts/default/partials/header/header';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-post',
@@ -10,5 +12,16 @@ import { Header } from '../../../../layouts/default/partials/header/header';
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  categories: any[] = [];
 
+  ngOnInit(): void {
+    this.userService.getCategories().subscribe((res) => {
+      this.categories = res.rows;
+    })
+  }
 }
--- frontend/src/app/modules/user/service/user.service.ts
+++ frontend/src/app/modules/user/service/user.service.ts
@@ -8,23 +8,27 @@ import { map, Observable } from "rxjs";
 
 export class UserService {
 
-    constructor(private backendService: BackendService) {}
+    constructor(private backendService: BackendService) { }
 
     getPostByUser(userId: number, page: number = 1, limit: number = 10): Observable<any> {
         return this.backendService.get(`post?user_id=${userId}&page=${page}&limit=${limit}`).pipe(
             map((result: any) => result?.data ?? null)
         );
     }
-
+    getCategories(): Observable<any> {
+        return this.backendService.get(`public/category`).pipe(
+            map((result: any) => result?.data ?? null)
+        );
+    }
     createPost(userId: number, post: any) {
-       //   return this.backendService.post<any>(`/users/${userId}/posts`, post);
+        //   return this.backendService.post<any>(`/users/${userId}/posts`, post);
     }
 
-    editPost(userId: number, postId: number, post: any){
-       //   return this.backendService.put<any>(`/users/${userId}/posts/${postId}`, post);
+    editPost(userId: number, postId: number, post: any) {
+        //   return this.backendService.put<any>(`/users/${userId}/posts/${postId}`, post);
     }
 
-    deletePost(postId: number){
+    deletePost(postId: number) {
         //   return this.backendService.delete<void>(`/users/${userId}/posts/${postId}`);
         return this.backendService.delete(`post/delete/${postId}`).pipe(
             map((result: any) => result?.data ?? null)
