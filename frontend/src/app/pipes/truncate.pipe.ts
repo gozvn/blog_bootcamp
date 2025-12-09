48405a0ea77324888fe558894506f83fc6fbb8cb
Gozvn <mrgozvn@gmail.com>
2025-12-09 10:26:11 +0700

    Fix : stip tag HTMl

--- frontend/src/app/pipes/truncate.pipe.ts
+++ frontend/src/app/pipes/truncate.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ 
-    name: 'truncate',
-    standalone: true, 
+@Pipe({
+  name: 'truncate',
+  standalone: true,
 })
 export class TruncatePipe implements PipeTransform {
+
+  private stripHtml(html: string): string {
+    return html.replace(/<[^>]*>/g, ''); // loại bỏ toàn bộ tag HTML
+  }
+
   transform(value: string, limit = 100, trail = '...'): string {
     if (!value) return '';
-    return value.length > limit ? value.substring(0, limit) + trail : value;
+
+    const plainText = this.stripHtml(value); // lấy text thuần
+
+    return plainText.length > limit
+      ? plainText.substring(0, limit) + trail
+      : plainText;
   }
 }
